386731d9ab7eb838fd4a1efb1aef19ac7d6730e2
Jenny Traer <lunalikesart@gmail.com>
2016-01-28 01:36:04 +0000

    Generate users controller Set up routes to user profile page Change routes.rb file Define show in controller Create new user view file Add user page link to top nav bar

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'LunaLikesArt'
+  config.mailer_sender = 'Nomster'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
